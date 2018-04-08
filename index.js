8f6e24850a0ecc1c768c6b7f0364ed87d82cd190
Aaron Master <mastermindz303@gmail.com>
2018-04-08 17:10:50 -0400

    src file

---
+++ index.js
@@ -0,0 +1,8 @@
+import { AppRegistry, Text } from 'react-native';
+import React from 'react';
+
+const App = () => (
+  <Text>Hello World</Text>
+);
+
+AppRegistry.registerComponent('NashuaNorthApp', () => App);
