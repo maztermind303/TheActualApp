78dd862c7e32fcf84d8e42186070ccf0f51fe19f
jmasaki24 <jamie.m.maddock@gmail.com>
2018-04-08 18:20:58 -0400

    lol

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Card, CardSection, Input, Button } from './common';
 
 class App extends Component {
-  render()
+  state = { screen: 'Home' }
+
+  render() {
     return(
       <View>
-        <View>
+        <Card>
           <Button />
-          <Text> NHSN </Text>
-        </View>
-
+          <Text style={styles.headerStyle}> NHSN </Text>
+        </Card>
+        <Card>
+          <HomePage />
+        </Card>
       </View>
     );
+  }
 }
+
+const styles = {
+  headerStyle: {
+    margin: 20,
+    padding: 10,
+    fontSize: 10
+  },
+};
 
 export default App;
