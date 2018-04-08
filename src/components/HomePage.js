08f57b13150967ecf8b416d7b26e2a1a25e8b484
jmasaki24 <jamie.m.maddock@gmail.com>
2018-04-08 18:21:30 -0400

    ok

---
+++ src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Listview } from 'react-native';
+
+class HomePage extends Component {
+  render() {
+    return (
+      <Listview />
+    );
+  }
+}
+
+export default HomePage;
