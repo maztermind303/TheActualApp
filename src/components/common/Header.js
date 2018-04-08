d76126b58ba499f703cc1a5b869b24470ea44747
jmasaki24 <jamie.m.maddock@gmail.com>
2018-04-08 17:15:18 -0400

    skills

---
+++ src/components/common/Header.js
@@ -0,0 +1,36 @@
+//import libraries
+import React from 'react';
+import { Text, View } from 'react-native';
+
+//make a Component
+const Header = (props) => {
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{props.headerText}</Text>
+    </View>
+  );
+};
+
+const styles = {
+  textStyle: {
+          fontSize: 20
+        },
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 10,
+    paddingBottom: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2, },
+    shadowOpacity: 0.9,
+    elevation: 2,
+    position: 'relative',
+  }
+};
+
+//make component available to other parts of the App
+export { Header };
